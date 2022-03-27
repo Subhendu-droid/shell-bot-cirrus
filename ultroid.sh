7b430a131fffc2abcf552db2e0147673347dea67
Subhendu-droid <55965922+Subhendu-droid@users.noreply.github.com>
2022-03-27 21:26:04 +0530

    Update ultroid.sh

--- ultroid.sh
+++ ultroid.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/TeamUltroid/Ultroid Ultroid
 cp PR/.env Ultroid
 cd Ultroid
-pip3 install --no-cache-dir -r Ultroid/requirements.txt
-pip3 install av --no-binary av
-python3 -m pyUltroid
+pip install --no-cache-dir -r Ultroid/requirements.txt
+pip install av --no-binary av
+bash startup
