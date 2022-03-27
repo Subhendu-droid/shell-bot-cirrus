0b2453ccf8738ddce46ef1a20b0312eaae465141
Subhendu-droid <55965922+Subhendu-droid@users.noreply.github.com>
2022-03-27 21:44:09 +0530

    Update ultroid.sh

--- ultroid.sh
+++ ultroid.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/Subhendu-droid/Ultroid Ultroid
 cp /home/circleci/project/PR/.env /home/circleci/project/bot/Ultroid
 cd Ultroid
-pwd
-ls
+pip install -U -r requirements.txt
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
+docker run --privileged --env-file .env --rm -i ultroid
