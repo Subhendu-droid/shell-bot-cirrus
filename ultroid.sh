a004a32da0757ee6c2cffee91c5edd3249dbf532
Subhendu-droid <55965922+Subhendu-droid@users.noreply.github.com>
2022-03-27 20:57:10 +0530

    Update ultroid.sh

--- ultroid.sh
+++ ultroid.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/TeamUltroid/Ultroid Ultroid
-cp /home/circleci/project/PR/.env /home/circleci/project/bot/Ultroid/.env
+cp /home/circleci/project/PR/.env /home/circleci/project/bot/Ultroid
 cd Ultroid
 pip install -U -r requirements.txt
 bash startup
